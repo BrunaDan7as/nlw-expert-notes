f992c43a83807c12b73e538d411dd94e3efcd2f3
Bruna Dantas <b.dantassoares@gmail.com>
2024-02-11 00:27:31 -0300

    Modais feitos.

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,8 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+
+
 export function App() {
  return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -18,7 +20,10 @@ export function App() {
     <div className='h-px bg-slate-700' />
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard/>
-        <NoteCard/>
+        <NoteCard note={{
+            date: new Date(),
+            content: 'Hello World'
+          }} />
       </div>
     </div>
   )
